6b1efc0a846bcea8a6b1f79e18e05173a5e8ee4e
lucatrazzi <luca.trazzi@adesso.ch>
2022-12-12 09:16:06 +0100

    day 4 part1

--- day4/4_test.go
+++ day4/4_test.go
@@ -6,15 +6,20 @@ import (
 )
 
 func TestExampleInput4(t *testing.T) {
-	ex := ``
+	ex := `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
 
-	shared.HelperTest(Day4_1, ex, 1, t)
-	shared.HelperTest(Day4_2, ex, 1, t)
+	shared.HelperTest(Day4_1, ex, 2, t)
+	shared.HelperTest(Day4_2, ex, 4, t)
 }
 
 func TestRealInput4(t *testing.T) {
 	content := shared.ReadFile("input")
 
-	shared.HelperTest(Day4_1, content, 1, t)
+	shared.HelperTest(Day4_1, content, 444, t)
 	shared.HelperTest(Day4_2, content, 1, t)
 }
